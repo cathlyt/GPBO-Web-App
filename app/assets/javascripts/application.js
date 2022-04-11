260d16db53b895d6566737dd74cc5a8dc8bc5592
Cathryn Lin <ylin4@andrew.cmu.edu>
2022-04-11 00:26:29 -0400

    flash partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,9 @@
 // @import "materialize";
 // @import "https://fonts.googleapis.com/icon?family=Material+Icons";
 
+// Flash fade
+$(function() {
+    $('.alert-box').fadeIn('normal', function() {
+       $(this).delay(3700).fadeOut();
+    });
+ });
