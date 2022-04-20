d3aa89a118aac533b3c84fe912751edcdd83c24c
Cathryn Lin <ylin4@andrew.cmu.edu>
2022-04-20 12:21:02 -0400

    item price else statement

--- app/controllers/item_prices_controller.rb
+++ app/controllers/item_prices_controller.rb
@@ -7,13 +7,13 @@ class ItemPricesController < ApplicationController
 
     def create
         @item_price = ItemPrice.new(item_price_params)
-        @item_price.start_date = Date.tomorrow
         @item = Item.find(params[:item_id])
         if @item_price.save
           flash[:notice] = "Successfully updated the price."
           redirect_to item_path(ItemPrice.last.item)
         else
-          render action: 'new'
+          @item = Item.find(params[:item_id])
+          render action: 'new', locals:{item: @item}
         end
     end
 
