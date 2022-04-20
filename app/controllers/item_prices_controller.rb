fa52da7b68f124d4e149c3ff152e2894b2a96401
Cathryn Lin <ylin4@andrew.cmu.edu>
2022-04-20 14:46:23 -0400

    item price create statement

--- app/controllers/item_prices_controller.rb
+++ app/controllers/item_prices_controller.rb
@@ -7,12 +7,12 @@ class ItemPricesController < ApplicationController
 
     def create
         @item_price = ItemPrice.new(item_price_params)
-        @item = Item.find(params[:item_id])
+        @item = Item.find(params[:item_price][:item_id])
         if @item_price.save
           flash[:notice] = "Successfully updated the price."
           redirect_to item_path(ItemPrice.last.item)
         else
-          @item = Item.find(params[:item_id])
+          
           render action: 'new', locals:{item: @item}
         end
     end
